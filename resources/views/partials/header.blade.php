9f06a4b950a9369a1d76dd5a5c334e6ba6558005
Andrea <andreamarongiu05@gmail.com>
2021-05-12 14:40:06 +0200

    Inizio header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,67 @@
+<header>
+  <div class="top-side">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a  href="#">DC Power℠ Visa®</a>
+          </li>
+          <li>
+            <a  href="#">Additional DC Sites ▼</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
+  <div class="bottom-side">
+    <div class="container">
+
+      {{-- Logo --}}
+      <div class="brand">
+        <img src="{{ asset("images/dc-logo.png")}}" alt="">
+      </div>
+
+      {{-- Nav link --}}
+      <nav>
+        <ul>
+          <li>
+            <a  href="#">Characters</a>
+          </li>
+          <li>
+            <a  href="#">Comics</a>
+          </li>
+          <li>
+            <a  href="#">Movies</a>
+          </li>
+          <li>
+            <a  href="#">tv</a>
+          </li>
+          <li>
+            <a  href="#">games</a>
+          </li>
+          <li>
+            <a  href="#">collectibles</a>
+          </li>
+          <li>
+            <a  href="#">videos</a>
+          </li>
+          <li>
+            <a  href="#">fans</a>
+          </li>
+          <li>
+            <a  href="#">news</a>
+          </li>
+          <li>
+            <a  href="#">shop ▾</a>
+          </li>
+        </nav>
+
+        {{-- Searchbar --}}
+        <div class="searchbar">
+          <input type="search" name="" value="">
+        </div>
+
+      </div>
+    </div>
+  </header>
